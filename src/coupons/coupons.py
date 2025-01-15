f6a6fefc0bc72b290157d324b0fe98606a6786a9
Elsabet Yemane Girma <elayemu@gmail.com>
2025-01-15 14:59:35 +0300

    Implemented coupon management system

---
+++ src/coupons/coupons.py
@@ -0,0 +1,6 @@
+"from flask import Blueprint, request" 
+"coupons = Blueprint('coupons', __name__)" 
+"@coupons.route('/coupons')" 
+"def list_coupons():" 
+"    coupons = get_coupons()" 
+"    return render_template('coupons.html', coupons=coupons)" 
